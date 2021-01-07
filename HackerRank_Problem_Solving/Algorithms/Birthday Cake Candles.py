d4c5b695f7f39d2bb701330bd5eee2a7e3496213
Ansh mishra <anshmishra9415@gmail.com>
2021-01-08 00:46:44 +0530

    algorithm

---
+++ HackerRank_Problem_Solving/Algorithms/Birthday Cake Candles.py
@@ -0,0 +1,34 @@
+
+import math
+import os
+import random
+import re
+import sys
+from collections import Counter
+
+#
+# Complete the 'birthdayCakeCandles' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY candles as parameter.
+#
+
+def birthdayCakeCandles(candles):
+
+    result=Counter(candles)
+    a=result.most_common(1)[0][0]
+    return candles.count(a)
+    # Write your code here
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    candles_count = int(input().strip())
+
+    candles = list(map(int, input().rstrip().split()))
+
+    result = birthdayCakeCandles(candles)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
