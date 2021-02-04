87676138e8aa91de94a71322d81bb42da625cae1
Ansh mishra <anshmishra9415@gmail.com>
2021-02-04 20:14:31 +0530

    leetcode

---
+++ Leetcode Practice/Palindrome Number.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if(x<0){
+            return false;
+        }
+        else{
+            int temp=x;
+            long rev = 0;
+            while(temp>0){
+                rev = rev*10 + temp%10;
+                temp/=10;
+            }
+            if(x==rev){
+                return true;
+            }
+            else{
+                return false;
+            }
+        }
+        
+    }
+};
