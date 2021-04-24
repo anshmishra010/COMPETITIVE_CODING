360959d69313134ecbdf78551fa9ac64d54c5c04
Ansh mishra <anshmishra9415@gmail.com>
2021-04-24 11:18:47 +0530

    two pointer

---
+++ Leetcode Practice/Linked List Cycle II.cpp
@@ -0,0 +1,19 @@
+ListNode* slow=head;
+        ListNode* fast=head;
+		while(slow!=NULL && fast!=NULL && fast->next!=NULL)
+		{
+			slow=slow->next;
+			fast=fast->next->next;
+			if(slow==fast)
+			{
+				slow=head;
+				while(slow!=fast)
+				{
+					slow=slow->next;
+					fast=fast->next;
+				}
+                return slow;
+			}
+		}
+		return NULL;
+    }
