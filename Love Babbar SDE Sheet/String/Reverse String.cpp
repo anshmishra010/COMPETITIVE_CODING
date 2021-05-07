4afc0a49e1e9b97352c1a8d9031640b2d2bd7d2d
Ansh mishra <anshmishra9415@gmail.com>
2021-05-07 21:09:04 +0530

    string

---
+++ Love Babbar SDE Sheet/String/Reverse String.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        int i= 0;
+        int j = s.size() -1;
+        //iterative way
+        while(i<j)
+        {
+            char temp = s[i];
+            s[i] = s[j];
+            s[j] = temp;
+            i++;
+            j--;
+        }
+    }
+};
+// we can also use inbuilt swap 
