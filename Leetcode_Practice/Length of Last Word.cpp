37116c6dd612aab8358d17f05e02ca811680bb0e
Ansh mishra <anshmishra9415@gmail.com>
2021-06-11 11:41:34 +0530

    brute force

--- Leetcode_Practice/Length of Last Word.cpp
+++ Leetcode_Practice/Length of Last Word.cpp
@@ -1,3 +1,11 @@
+/*
+Given a string s consists of some words separated by spaces, return the length of the last word in the string. If the last word does not exist, return 0.
+
+A word is a maximal substring consisting of non-space characters only.
+*/
+//https://leetcode.com/problems/length-of-last-word/
+
+
 class Solution {
 public:
     int lengthOfLastWord(string s) {
