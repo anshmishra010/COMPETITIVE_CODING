957ed27b334e4972d716fb01480538a3757a727b
Ansh mishra <anshmishra9415@gmail.com>
2021-06-19 20:44:21 +0530

    prefix sum

---
+++ Demux_Academy_codes/Week 1/Range Sum Query - Immutable.cpp
@@ -0,0 +1,52 @@
+/*
+Given an integer array nums, handle multiple queries of the following type:
+
+Calculate the sum of the elements of nums between indices left and right inclusive where left <= right.
+Implement the NumArray class:
+
+NumArray(int[] nums) Initializes the object with the integer array nums.
+int sumRange(int left, int right) Returns the sum of the elements of nums between indices left and right inclusive (i.e. nums[left] + nums[left + 1] + ... + nums[right]).
+ 
+
+Example 1:
+
+Input
+["NumArray", "sumRange", "sumRange", "sumRange"]
+[[[-2, 0, 3, -5, 2, -1]], [0, 2], [2, 5], [0, 5]]
+Output
+[null, 1, -1, -3]
+
+Explanation
+NumArray numArray = new NumArray([-2, 0, 3, -5, 2, -1]);
+numArray.sumRange(0, 2); // return (-2) + 0 + 3 = 1
+numArray.sumRange(2, 5); // return 3 + (-5) + 2 + (-1) = -1
+numArray.sumRange(0, 5); // return (-2) + 0 + 3 + (-5) + 2 + (-1) = -3
+*/
+
+
+class NumArray {
+public:
+    
+    vector<int> res = {0};
+    NumArray(vector<int>& nums) {
+        int total = 0;
+        
+        for(int x : nums){
+            // we will be sotring the sum of the elements like a prefix sum
+            total += x;
+            res.push_back(total);
+            
+        }
+    }
+    
+    int sumRange(int left, int right) {
+        // here we are using a simple mathematical logic ,
+        return res[right+1] - res[left];
+    }
+};
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * NumArray* obj = new NumArray(nums);
+ * int param_1 = obj->sumRange(left,right);
+ */
